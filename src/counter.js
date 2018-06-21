8880388ffb38fc6944d8998b07fe7facf66fec1b
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2018-06-21 16:43:35 -0500

    fixed compile issues

--- src/counter.js
+++ src/counter.js
@@ -2,11 +2,13 @@
 
 import React, {Component} from 'react';
 import Delta from './delta';
+//import MyContext from './context';
 
 import './counter.css';
 
 type PropsType = {
-  counter: number
+  counter: number,
+  dispatch: Function
 };
 
 class Counter extends Component<PropsType> {
